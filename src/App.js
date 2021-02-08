a851388332ccaa2c4b4e5d2da2d0cd9bd5cc0354
Jeronimo Ortega <jaod0814@gmail.com>
2021-02-08 14:44:47 -0500

    ver 1.0.1

--- src/App.js
+++ src/App.js
@@ -8,20 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          Hello Eldi
-=======
-          Edit <code>src/App.js</code> and save to reload.
->>>>>>> parent of 67bec64... Hello World
+          Hola Mundo 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
